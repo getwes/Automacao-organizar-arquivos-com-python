9b88a14f6b3771235d8064270e1c11dd37fb5e1f
gitwes <wesleyfreire1707@gmail.com>
2024-02-25 15:36:05 -0300

    Locais

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ import os
 from tkinter.filedialog import askdirectory
 
 caminho = askdirectory(title="selecione uma pasta")
-print(caminho)
+
+lista_arquivos = os.listdir(caminho)
+print(lista_arquivos)
+
+locais = {
+    "imagens": [".png", ".jpg"],
+    "planilhas": [".exe", ".rar", ".zip", ".xlsx", "docx", ".msi"],
+    "pdfs": [".pdf"],
+    "csv": [".csv"]
+}
